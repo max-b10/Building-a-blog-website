368dea2da2451a63022977eb91b4c4644c3d88b8
max-b10 <bungaymax@gmail.com>
2021-03-03 15:49:21 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links");
+
+hamburger.addEventListener("click", (e) => {
+  console.log("clicked the menu dropdown");
+});
+
+console.log("app.js running");
